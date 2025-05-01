224fbe751d83c1bb69bdd0255c28b30242046ab8
Pritam Das <pritamdas.078@kathford.edu.np>
2025-05-01 20:07:18 +0545

    Database Connected to Atlas

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./config/db");
 const app = express();
 
 dotenv.config();
+connectDB();
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors);
 
-app.use('/api/v1/test')
+// app.use("/api/v1/test",);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
