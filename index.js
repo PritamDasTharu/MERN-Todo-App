1a3d105567ab405ead0d470616e6f5b967d7a1da
Pritam Das <pritamdas.078@kathford.edu.np>
2025-05-05 07:14:57 +0545

    Added delete and update API in backend

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const connectDB = require("./config/db");
 const app = express();
 
 const userRoute = require("./routes/userRoute");
+const todoRoute = require("./routes/todoRoutes");
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,7 @@ app.use(morgan("dev"));
 app.use(cors());
 
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/todo", todoRoute);
 app.use("/api/v1/test", require("./routes/testRoutes"));
 
 const PORT = process.env.PORT;
